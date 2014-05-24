c3fac74d6f179c7dae06349be9a7df0fdbadb97c
alexander sanchez <asanchez.dev@gmail.com>
2014-05-24 05:59:46 -0400

    asdf

--- zonokai-theme.el
+++ zonokai-theme.el
@@ -29,7 +29,7 @@
 
 ;;; Commentary:
 ;;
-;; my personal touch on the emacs monokai theme, with more a blue
+;; my personal touch on the Emacs monokai theme, with more a blue
 ;; base to the theme, with green and different hues of blue and red
 ;; for accenting
 ;;
