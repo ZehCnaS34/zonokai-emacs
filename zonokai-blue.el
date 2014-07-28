2e0aea0304bd41a1be15ced99c2562aaaa5c685a
Alexander Sanchez <asanchez.dev@gmail.com>
2014-07-27 22:00:56 -0400

    `zonokai-blue' and `zonokai-red'

---
+++ zonokai-blue.el
@@ -0,0 +1,9 @@
+(require 'zonokai)
+
+(deftheme zonokai-blue "Blue version of zonokai")
+(create-zonokai-theme nil 'zonokai-blue)
+
+
+(provide-theme 'zonokai-blue)
+
+
---
+++ zonokai-red.el
@@ -0,0 +1,7 @@
+(require 'zonokai)
+
+(deftheme zonokai-red "Red version of zonokai")
+(create-zonokai-theme t 'zonokai-red)
+
+
+(provide-theme 'zonokai-red)
